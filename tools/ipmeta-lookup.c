fa7d25cae697ec10f5f7c199dd7682540bc3f6d9
rodricfanou <roderick@caida.org>
2019-09-19 18:56:52 -0700

    Marking the lookup function;

--- tools/ipmeta-lookup.c
+++ tools/ipmeta-lookup.c
@@ -64,7 +64,7 @@ static void lookup(char *addr_str, iow_t *outfile)
   uint8_t mask;
   int i;
 
-  printf('stdout', 'Entering a lookup function');
+  printf("Entering a lookup function");
 
   /* preserve the original string for dumping */
   strcpy(orig_str, addr_str);
